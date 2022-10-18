9e7bfe735cc6dfc5d16ab1f4a2d20b8b65c103d9
ZBGC <105749803+ZBGC@users.noreply.github.com>
2022-10-18 15:56:18 -0400

    java test

---
+++ src/test.java
@@ -0,0 +1,5 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("whilst I usually use Intellij for java, just testing out its functionalities on VSCode Studio, nothing too big");
+    }
+}
